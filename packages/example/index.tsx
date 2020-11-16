865e13b09c78033e035bd64d03c5a4aa5d0d37d2
forsigner <forsigner@gmail.com>
2020-11-16 15:18:24 +0800

    initial commit

---
+++ packages/example/index.tsx
@@ -0,0 +1,15 @@
+// import * as React from "react";
+// import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Button } from "@bone-ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
