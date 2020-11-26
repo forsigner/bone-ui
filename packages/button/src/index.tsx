6728b0a45d5e93498a68f8196896917af2a7e4e3
Thomas Trainset <gotoanything@foxmail.com>
2020-11-26 15:58:50 +0800

    feat: Button

--- packages/button/src/index.tsx
+++ packages/button/src/index.tsx
@@ -1,5 +1,108 @@
-import React from "react";
+import React, { forwardRef } from 'react'
+import { Button as StyleButton } from '@styli/react'
+import { StyliHTMLProps } from '@styli/types'
+import { getColorValue } from '@styli/core'
 
-export const Button = () => {
-  return <div>Button</div>;
-};
+type ButtonSize = 'xs' | 'sm' | 'md' | 'lg'
+type ButtonVariant = 'solid' | 'outline' | 'ghost' | 'link'
+
+export interface ButtonProps extends StyliHTMLProps<'div'> {
+  text: string
+  colorScheme?: string
+  size?: ButtonSize
+  variant?: ButtonVariant
+  leftIcon?: React.ReactElement
+  rightIcon?: React.ReactElement
+  disabled?: boolean
+}
+
+export const Button = forwardRef<HTMLDivElement, ButtonProps>((props, ref) => {
+  const { size = 'md', variant = 'solid', text, leftIcon, rightIcon, colorScheme = 'blue50', disabled, ...rest } = props
+  const atomicProps: any = rest
+
+  return (
+    <StyleButton
+      ref={ref}
+      centerY
+      bg="none"
+      border="0"
+      outlineNone
+      cursorPointer
+      appearance="none"
+      userSelect="none"
+      rounded="0.375rem"
+      whiteSpace="nowrap"
+      verticalAlign="middle"
+      transition="all 250ms ease 0s"
+      opacity-40={disabled}
+      cursorNotAllowed={disabled}
+      {...getVariantMap(colorScheme, disabled)[variant]}
+      {...getSizeMap()[size]}
+      {...atomicProps}
+    >
+      {leftIcon}
+      {text}
+      {rightIcon}
+    </StyleButton>
+  )
+})
+
+function getVariantMap(color: string, disabled = false) {
+  return {
+    solid: {
+      white: true,
+      bgColor: color,
+      ...disabled ? {} : {
+        'bgColor--hover': `${color}-D10`,
+        'bgColor--active': `${color}-D10`,
+        'shadow--focus': `0 0 0 .2rem ${getColorValue(color + '-T50')}`,
+      },
+    },
+    ghost: {
+      color,
+      ...disabled ? {} : {
+        'bgColor--hover': `${color}-T90`,
+        'bgColor--active': `${color}-T90`,
+        'shadow--focus': `0 0 0 .2rem ${getColorValue(color + '-T50')}`,
+      }
+    },
+    outline: {
+      color,
+      border: `1px solid ${getColorValue(color)}`,
+      ...disabled ? {} : {
+        'bgColor--hover': `${color}-T90`,
+        'bgColor--active': `${color}-T90`,
+        'shadow--focus': `0 0 0 .2rem ${getColorValue(color + '-T50')}`,
+      }
+    },
+  }
+}
+
+function getSizeMap() {
+  return {
+    xs: {
+      h: '1.5rem',
+      minW: '1.5rem',
+      f0: true,
+      px: '0.5rem',
+    },
+    sm: {
+      h: '2rem',
+      minW: '2rem',
+      f1: true,
+      px: '0.75rem',
+    },
+    md: {
+      h: '2.5rem',
+      minW: '2.5rem',
+      f2: true,
+      px: '1rem',
+    },
+    lg: {
+      h: '3rem',
+      minW: '3rem',
+      f3: true,
+      px: '1.25rem',
+    }
+  }
+}
---
+++ packages/example/components/ButtonDemo.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { View, Heading } from '@styli/react'
+import { Button } from '@bone-ui/Button'
+import { AcademicCapOutline } from '@bone-ui/icons'
+
+export const ButtonDemo = () => {
+  return (
+    <View>
+      <Heading>Size</Heading>
+      <View row spaceX-20>
+        <Button text="Button" size="xs" />
+        <Button text="Button" size="sm" />
+        <Button text="Button" size="md" />
+        <Button text="Button" size="lg" />
+      </View>
+
+      <Heading>variant</Heading>
+      <View row spaceX-20>
+        <Button text="Button" variant="outline" />
+        <Button text="Button" variant="ghost" />
+        <Button text="Button" variant="solid" />
+      </View>
+
+      <Heading>colorScheme</Heading>
+      <View row spaceX-20>
+        <Button text="Button" colorScheme="red50" />
+        <Button text="Button" colorScheme="red80" />
+        <Button text="Button" colorScheme="red" />
+      </View>
+
+      <Heading>with icon</Heading>
+      <View row spaceX-20>
+        <Button text="Button" leftIcon={<AcademicCapOutline mr1 />} />
+        <Button text="Button" rightIcon={<AcademicCapOutline ml1 />}  />
+        <Button text="Button" leftIcon={<AcademicCapOutline mr1 />} rightIcon={<AcademicCapOutline ml1/>} />
+      </View>
+
+      <Heading>disable</Heading>
+      <View row spaceX-20>
+        <Button text="Button" disabled />
+      </View>
+    </View>
+  )
+}
