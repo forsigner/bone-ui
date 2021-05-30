828276fcb8d175a722025e4edc2aef9da473e1cf
forsigner <forsigner@gmail.com>
2021-05-30 21:29:06 +0800

    feat(portal): can use portal in ssr

---
+++ packages/portal/src/index.tsx
@@ -0,0 +1,26 @@
+import React, { Box } from '@fower/react'
+import { FowerHTMLProps } from '@fower/types'
+import { FC, useEffect, useState } from 'react'
+import { createPortal } from 'react-dom'
+
+export interface PortalProps extends FowerHTMLProps<'div'> {}
+
+export const Portal: FC<PortalProps> = (props) => {
+  const { children, ...rest } = props
+  const [mounted, setMounted] = useState(false)
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if (typeof document === 'undefined') return null
+
+  return mounted
+    ? createPortal(
+        <Box className="bone-ui-portal" {...rest}>
+          {children}
+        </Box>,
+        document.body,
+      )
+    : null
+}
