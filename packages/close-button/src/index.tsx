8b42d8c93d87347baff1b033a8200c2ef3494877
forsigner <forsigner@gmail.com>
2021-05-30 16:14:40 +0800

    feat(close-button): rm bg color

--- packages/close-button/src/index.tsx
+++ packages/close-button/src/index.tsx
@@ -1,11 +1,11 @@
 import React, { forwardRef } from 'react'
-import { styled } from '@styli/styled'
-import { StyliHTMLProps } from '@styli/types'
+import { styled } from '@fower/styled'
+import { FowerHTMLProps } from '@fower/types'
 import { XOutline } from '@bone-ui/icons/lib/XOutline'
 
 const Button = styled('button')
 
-export interface CloseButtonProps extends StyliHTMLProps<'button'> {
+export interface CloseButtonProps extends FowerHTMLProps<'button'> {
   size?: number
 }
 
@@ -16,19 +16,19 @@ export const CloseButton = forwardRef<HTMLButtonElement, CloseButtonProps>((prop
       ref={ref}
       aria-label="Close"
       className="bone-close-button"
-      center
+      toCenter
       rounded={size * 0.2}
       cursorPointer
       p0
-      bg="none"
-      bgBlack-T94--hover
-      s={size}
+      bgTransparent
+      bgBlack--T94--hover
+      square={size}
       outlineNone
-      border="none"
-      css={{ transition: 'all 0.3s' }}
+      borderNone
+      css={{ transition: 'background-color 0.3s' }}
       {...rest}
     >
-      <XOutline opacity-80 s={size * 0.7}></XOutline>
+      <XOutline opacity-80 square={size * 0.7}></XOutline>
     </Button>
   )
 })
