f25de340152854c30b4c34c793ee5783694f51e3
forsigner <forsigner@gmail.com>
2021-06-05 15:42:38 +0800

    feat: improve divider style

--- packages/divider/src/index.tsx
+++ packages/divider/src/index.tsx
@@ -1,24 +1,3 @@
-import React, { FC } from 'react'
-import { forwardRef } from '@bone-ui/utils'
-import { FowerHTMLProps } from '@fower/types'
-import { Box } from '@fower/react'
-
-type Placement = 'top' | 'right' | 'bottom' | 'left'
-
-export interface DividerProps extends FowerHTMLProps<'div'> {
-  isOpened: boolean
-  label?: string | React.ReactElement
-  children?: any
-  bg?: string
-  className?: string
-  placement?: Placement
-}
-
-export const Divider: FC<DividerProps> = forwardRef((props: DividerProps, ref) => {
-  const { ...rest } = props
-  return (
-    <Box ref={ref} className="bone-ui-divider" {...rest}>
-      <Box>....</Box>
-    </Box>
-  )
-})
+export * from './Divider'
+export * from './DividerTitle'
+export * from './context'
