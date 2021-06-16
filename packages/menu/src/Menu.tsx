8af6bcd56e25f078101bb618db513aa233aa02aa
forsigner <forsigner@gmail.com>
2021-06-16 14:41:49 +0800

    feat(menu): improve menu types

--- packages/menu/src/Menu.tsx
+++ packages/menu/src/Menu.tsx
@@ -1,20 +1,25 @@
-import React, { forwardRef } from 'react'
+import React, { FC } from 'react'
+import { forwardRef } from '@bone-ui/utils'
 import { Box } from '@fower/react'
 import { FowerHTMLProps } from '@fower/types'
+import { MenuContext, MenuProvider } from './context'
 
-export interface MenuProps extends FowerHTMLProps<'div'> {}
+export interface MenuProps extends MenuContext, FowerHTMLProps<'div'> {}
 
-export const Menu = forwardRef<HTMLDivElement, MenuProps>((props, ref) => {
+export const Menu: FC<MenuProps> = forwardRef((props: MenuProps, ref) => {
+  const { colorScheme = 'brand500', showCheckIcon = true, checkIcon, ...rest } = props
   return (
-    <Box
-      className="bone-menu"
-      ref={ref}
-      bgWhite
-      minW-140
-      rounded-4
-      overflowHidden
-      shadow
-      {...props}
-    />
+    <MenuProvider value={{ colorScheme, showCheckIcon, checkIcon }}>
+      <Box
+        className="bone-menu"
+        ref={ref}
+        bgWhite
+        minW-140
+        rounded
+        overflowHidden
+        shadow
+        {...rest}
+      />
+    </MenuProvider>
   )
 })
--- packages/menu/src/MenuItem.tsx
+++ packages/menu/src/MenuItem.tsx
@@ -1,28 +1,38 @@
-import React, { forwardRef } from 'react'
-import { Box } from '@fower/react'
+import React, { FC } from 'react'
+import { forwardRef } from '@bone-ui/utils'
+import { CheckOutline } from '@bone-ui/icons/lib/CheckOutline'
 import { FowerHTMLProps } from '@fower/types'
+import { Box } from '@fower/react'
+import { useMenuContext } from './context'
 
 export interface MenuItemProps extends FowerHTMLProps<'div'> {
   selected?: boolean
 }
 
-export const MenuItem = forwardRef<HTMLDivElement, MenuItemProps>((props, ref) => {
-  const { selected, ...rest } = props
+export const MenuItem: FC<MenuItemProps> = forwardRef((props: MenuItemProps, ref) => {
+  const { selected, children, ...rest } = props
+  const { colorScheme, showCheckIcon, checkIcon } = useMenuContext()
+  if (selected) rest.color = colorScheme
+
+  const icon = checkIcon ? checkIcon : <CheckOutline size={18} />
+
   return (
     <Box
       className="bone-menu-item"
       ref={ref}
-      toLeft
-      px-12
-      py-8
+      px3
+      minH9
       toCenterY
-      green400={selected}
+      toBetween
       gray800={!selected}
       cursorPointer
-      spaceX3
       bgGray100--hover
-      css={{ transition: 'background-color 0.4s' }}
+      transitionCommon
+      duration-400
       {...rest}
-    />
+    >
+      <Box>{children}</Box>
+      {showCheckIcon && selected && icon}
+    </Box>
   )
 })
