ac3eadb607a5a027f73f306a0ca84af3c1eb954f
forsigner <forsigner@gmail.com>
2021-06-16 15:11:49 +0800

    docs: improve MenuDemo

--- example/pages/MenuDemo.tsx
+++ example/pages/MenuDemo.tsx
@@ -1,11 +1,25 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Menu, MenuItem } from '@bone-ui/menu'
 import { Box } from '@fower/react'
 
 export default function MenuDemo() {
   return (
-    <Box p20>
+    <Box p20 w-400 spaceY2>
+      <Box as="h2">Baisc</Box>
       <Menu>
+        <MenuItem selected>Red</MenuItem>
+        <MenuItem selected>Green</MenuItem>
+        <MenuItem>Blue</MenuItem>
+      </Menu>
+
+      <Box as="h2">Color Scheme</Box>
+      <Menu colorScheme="red500">
+        <MenuItem>Red</MenuItem>
+        <MenuItem selected>Green</MenuItem>
+        <MenuItem>Blue</MenuItem>
+      </Menu>
+
+      <Menu colorScheme="pink500">
         <MenuItem>Red</MenuItem>
         <MenuItem selected>Green</MenuItem>
         <MenuItem>Blue</MenuItem>
